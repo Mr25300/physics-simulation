98a7737a0ce6cbb83cb6cfd300bb64c0864b54ae
Mr25300 <109829617+Mr25300@users.noreply.github.com>
2025-03-03 13:24:29 -0500

    fixed range css support for other browers

--- src/projectiles/material.ts
+++ src/projectiles/material.ts
@@ -1,9 +1,10 @@
-class Material {
+export class Material {
     constructor(
-        private _elasticity: number,
-        private _staticFriction: number,
-        private _kineticFriction: number,
-    ) {
-
-    }
+        public charge: number,
+        public elasticity: number,
+        public staticFriction: number,
+        public kineticFriction: number,
+        public drag: number,
+        public color: string
+    ) {}
 }
